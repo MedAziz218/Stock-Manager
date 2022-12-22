2a64bd5116f33fb4e3b81a8f29e060c4d17fe48a
Saif Sebai <121020631+Saif-Sebai@users.noreply.github.com>
2022-12-22 13:21:03 +0100

    Added history struct

--- Lib/structures.c
+++ Lib/structures.c
@@ -1,4 +1,19 @@
 
+
+ 
+typedef struct history {
+	int operation_type; // 1 means + and 0 means negative
+	long double date; //self explanatory
+	int product_id; //self explanatory
+	int quantity; //self explanatory
+	float unit_price; //self explanatory
+	float operation_price; //total positive/negative per unit
+} history;
+
+
+
+
+
 typedef struct product{
 	int id;
 	char name[64];
